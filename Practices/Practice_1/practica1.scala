3b7f35720abd9f8c6806bcc5e9e9987df0d95177
Ricardo <ricardo.jacuinde16@tectijuana.edu.mx>
2021-03-11 21:44:17 -0800

    puntos 4,5,6 de la practica 1

--- Practices/Practice_1/practica1.scala
+++ Practices/Practice_1/practica1.scala
@@ -32,3 +32,18 @@ if (primo == true)
 var bird: String = "tweet"
 println(s"Estoy escribiendo un ${bird}")
 
+// 4. Given the variable message = "Hello Luke, I am your father!" uses slilce to extract the
+val message = "Hola Luke yo soy tu padre"
+message slice  (5,10)
+
+//5. What is the difference between value and a variable in scala?
+// the object assigned to a val cannot be replaced, while an object assigned to a var can, that is, one is immutable and the other is not
+
+// 6. Given the tuple (2,4,5,1,2,3,3.1416,23) returns the number 3.1416
+
+val my_tup = (2,4,5,1,2,3,3.1416,23)
+
+my_tup._7
+
+
+
