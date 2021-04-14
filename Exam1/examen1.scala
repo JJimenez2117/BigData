907643d2a39546489dd899a8fd5c12a6cbad191f
Ricardo <ricardo.jacuinde16@tectijuana.edu.mx>
2021-04-14 16:39:08 -0700

    Add points 9 and 10

--- Exam1/examen1.scala
+++ Exam1/examen1.scala
@@ -9,3 +9,23 @@ df.describe().show()
 df.head(5)
 val df2 = df.withColumn("HV Ratio",df("High")/df("Volume"))
 df.select(max("Close")).show()
+
+
+
+
+
+//9) Escribe con tus propias palabras en un comentario de tu codigo. ¿Cuál es el
+//significado de la columna Cerrar “Close”?
+
+
+//Respuesta: es ultimo precio de cotizacion que se registro en el dia
+
+
+
+//10) ¿Cuál es el máximo y mínimo de la columna “Volume”?
+
+// maximo
+df.select(max("Volume")).show()
+
+// minimo
+df.select(min("Volume")).show()
